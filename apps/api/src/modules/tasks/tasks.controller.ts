6a76ec89ad21829c842c085035d229ee7c8415ab
Guilherme Ghirro <guighirro254@gmail.comgit config --global user.email guighirro254@gmail.com>
2026-04-04 13:34:36 -0300

    fix: fixed package start and compiler api, and updated libs and added nest-cli

--- apps/api/src/modules/tasks/tasks.controller.ts
+++ apps/api/src/modules/tasks/tasks.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
-import { TasksService } from './tasks.service'
-import { Task } from './tasks.interface'
+  import { Controller, Get, Post, Body } from '@nestjs/common'
+  import { TasksService } from './tasks.service'
+  import { Task } from './tasks.interface'
 
-@Controller('tasks')
-export class TasksController {
+  @Controller('tasks')
+  export class TasksController {
 
-  constructor(private service: TasksService) {
-    console.log('SERVICE:', service)
+   constructor(private readonly service: TasksService) {
+      console.log('SERVICE:', service)
+    }
+
+    @Get()
+    async findAll() {
+      return this.service.findAll()
+    }
+
+    @Post()
+    async create(@Body() task: Task) {
+      return this.service.create(task)
+    }
+
   }
-
-  @Get()
-  async findAll() {
-    return this.service.findAll()
-  }
-
-  @Post()
-  async create(@Body() task: Task) {
-    return this.service.create(task)
-  }
-
-}
